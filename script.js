40e528a5c10a345511a6f1ad1fb62da919ae3c0f
llokki <llokki92@gmail.com>
2024-05-15 19:08:12 +0500

    bag fix

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const apiUrl = "https://mysite.com/webhook/green-api/";
+  const apiUrl = "https://7103.api.greenapi.com";
   
-    function performGetRequest(endpoint) {
-      var idInstance = document.getElementById('idInstance').value;
-      var apiTokenInstance = document.getElementById('ApiTokenInstance').value;
-      var url = `${apiUrl}waInstance${idInstance}/${endpoint}/${apiTokenInstance}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById('serverResponse').value = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-          console.error('Ошибка:', error);
-          document.getElementById('serverResponse').value = 'Ошибка: ' + error;
-        });
-    }
-  
-    function performPostRequest(endpoint, bodyData) {
-      var idInstance = document.getElementById('idInstance').value;
-      var apiTokenInstance = document.getElementById('ApiTokenInstance').value;
-      var url = `${apiUrl}waInstance${idInstance}/${endpoint}/${apiTokenInstance}`;
-  
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bodyData)
+  function performGetRequest(endpoint) {
+    var idInstance = document.getElementById('idInstance').value;
+    var apiTokenInstance = document.getElementById('ApiTokenInstance').value;
+    var url = `${apiUrl}waInstance${idInstance}/${endpoint}/${apiTokenInstance}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('serverResponse').value = JSON.stringify(data, null, 2);
       })
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById('serverResponse').value = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-          console.error('Ошибка:', error);
-          document.getElementById('serverResponse').value = 'Ошибка: ' + error;
-        });
-    }
+      .catch(error => {
+        console.error('Ошибка:', error);
+        document.getElementById('serverResponse').value = 'Ошибка: ' + error;
+      });
+  }
   
-    document.getElementById('getSettings').addEventListener('click', () => performGetRequest('getSettings'));
+  function performPostRequest(endpoint, bodyData) {
+    var idInstance = document.getElementById('idInstance').value;
+    var apiTokenInstance = document.getElementById('ApiTokenInstance').value;
+    var url = `${apiUrl}waInstance${idInstance}/${endpoint}/${apiTokenInstance}`;
+  
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bodyData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById('serverResponse').value = JSON.stringify(data, null, 2);
+      })
+      .catch(error => {
+        console.error('Ошибка:', error);
+        document.getElementById('serverResponse').value = 'Ошибка: ' + error;
+      });
+  }
+  
+  document.getElementById('getSettings').addEventListener('click', () => performGetRequest('getSettings'));
     
-    document.getElementById('getStateInstance').addEventListener('click', () => performGetRequest('getStateInstance'));
+  document.getElementById('getStateInstance').addEventListener('click', () => performGetRequest('getStateInstance'));
   
-    document.getElementById('sendMessage').addEventListener('click', () => {
-      var message = document.getElementById('messageText').value;
-      var phoneNumber = document.getElementById('phoneForMessage').value;
-      performPostRequest('sendMessage', { phone: phoneNumber, message: message });
-    });
-  
-    document.getElementById('sendFileByUrl').addEventListener('click', () => {
-      var fileUrl = document.getElementById('urlRequest').value;
-      var phoneNumber = document.getElementById('phoneForFile').value;
-      performPostRequest('sendFileByUrl', { phone: phoneNumber, fileUrl: fileUrl });
-    });
-  
+  document.getElementById('sendMessage').addEventListener('click', () => {
+    var message = document.getElementById('messageText').value;
+    var phoneNumber = document.getElementById('phoneForMessage').value;
+    performPostRequest('sendMessage', { phone: phoneNumber, message: message });
   });
+  
+  document.getElementById('sendFileByUrl').addEventListener('click', () => {
+    var fileUrl = document.getElementById('urlRequest').value;
+    var phoneNumber = document.getElementById('phoneForFile').value;
+    performPostRequest('sendFileByUrl', { phone: phoneNumber, fileUrl: fileUrl });
+  });
+  
+});
